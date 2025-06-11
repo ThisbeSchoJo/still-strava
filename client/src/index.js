b7210c64f8a6c730e2f1e47e9663d33d0943a3ee
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-11 13:07:36 -0600

    Created styling folder to keep css files organized - moved index.css into folder and updated route in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import App from "./components/App";
-import "./index.css";
+import "./styling/index.css";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
