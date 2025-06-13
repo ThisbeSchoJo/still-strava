181e40c5c8855fe29cfc136719913ad2db23b8b9
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-12 22:15:48 -0600

    Added Comment model to models.py, imported Comment model into app.py, migrated (and fixed migration)

--- server/models.py
+++ server/models.py
@@ -12,6 +12,9 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, unique=True, nullable=False)
     image = db.Column(db.String, nullable=False)
 
+    activities = db.relationship('Activity', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+
     def __repr__(self):
         return f'<User {self.username}, Email: {self.email}, Image: {self.image}>'
     
@@ -23,6 +26,21 @@ class Activity(db.Model, SerializerMixin):
     description = db.Column(db.String)
     datetime = db.Column(db.DateTime)
     photos = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    comments = db.relationship('Comment', backref='activity', lazy=True)
     def __repr__(self):
         return f'<Activity {self.name}, Description: {self.description}, Datetime: {self.datetime}, Photos: {self.photos}>'
+
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    datetime = db.Column(db.DateTime)
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __repr__(self):
+        return f'<Comment {self.content}, Datetime: {self.datetime}, Activity: {self.activity_id}, User: {self.user_id}>'
+    
