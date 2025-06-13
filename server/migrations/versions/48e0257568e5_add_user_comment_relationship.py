3cde789f195db2fb644931df37edd10f779aab55
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-12 22:15:48 -0600

    Added Comment model to models.py, imported Comment model into app.py, migrated (and fixed migration)

---
+++ server/migrations/versions/48e0257568e5_add_user_comment_relationship.py
@@ -0,0 +1,24 @@
+"""Add User-Comment relationship
+
+Revision ID: 48e0257568e5
+Revises: 2e129c3fc412
+Create Date: 2025-06-12 22:13:54.120752
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '48e0257568e5'
+down_revision = '2e129c3fc412'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
