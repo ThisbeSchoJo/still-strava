09ebf6a1a0f07f5670f40880b5d1cc2825487ac7
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-12 22:43:10 -0600

    Added /activities route to get and post activities in app.py (AllActivities)

--- server/app.py
+++ server/app.py
@@ -18,10 +18,14 @@ from models import User, Activity, Comment
 def index():
     return '<h1>Still Strava</h1>'
 
+# CRUD for users
 class AllUsers(Resource):
     def get(self):
-        users = [user.to_dict() for user in User.query.all()]
-        return make_response(users, 200)
+        stmt = select(User)
+        result = db.session.execute(stmt)
+        users = result.scalars().all()
+        response_body = [user.to_dict(only=('id', 'username', 'email', 'image')) for user in users]
+        return make_response(response_body, 200)
     
     def post(self):
         try:
@@ -42,12 +46,43 @@ class AllUsers(Resource):
         
 api.add_resource(AllUsers, '/users')
 
-
 class UserById(Resource):
     def get(self, id):
         user = User.query.filter_by(id=id).first()
 
 api.add_resource(UserById, '/users/<int:id>')
+
+# CRUD for activities
+class AllActivities(Resource):
+    def get(self):
+        stmt = select(Activity)
+        result = db.session.execute(stmt)
+        activities = result.scalars().all()
+        response_body = [activity.to_dict(only=('id', 'name', 'description', 'datetime', 'photos')) for activity in activities]
+        return make_response(response_body, 200)
+    
+    def post(self):
+        try:
+            new_activity = Activity(
+                name=request.json.get('name'),
+                description=request.json.get('description'),
+                datetime=request.json.get('datetime'),
+                photos=request.json.get('photos')
+            )
+            db.session.add(new_activity)
+            db.session.commit()
+            response_body = new_activity.to_dict(only=('id', 'name', 'description', 'datetime', 'photos'))
+            return make_response(response_body, 201)
+        except Exception as e:
+            response_body = {
+                "error": str(e)
+            }
+            return make_response(response_body, 422)    
+        
+api.add_resource(AllActivities, '/activities')
+        
+    
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
