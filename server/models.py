c28910b578f0f7fb19856a5633aa51b5cd253645
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-12 22:23:57 -0600

    Added in validations for User, Activity, and Comment models in models.py

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
@@ -15,6 +16,28 @@ class User(db.Model, SerializerMixin):
     activities = db.relationship('Activity', backref='user', lazy=True)
     comments = db.relationship('Comment', backref='user', lazy=True)
 
+    serialize_rules = ('-activities.user', '-comments.user')
+
+    @validates('username', 'image')
+    def validate_username(self, column_name, value):
+        if type(value) != str:
+            raise TypeError(f"{column_name} must be a string")
+        elif len(value) < 5:
+            raise ValueError(f"{column_name} must be at least 5 characters long")
+        else:
+            return value
+
+    @validates('email')
+    def validate_email(self, email):
+        if type(email) != str:
+            raise TypeError("Email must be a string")
+        elif '@' not in email:
+            raise ValueError("Invalid email address")
+        elif len(email) < 10:
+            raise ValueError("Email must be at least 10 characters long")
+        else:
+            return email
+
     def __repr__(self):
         return f'<User {self.username}, Email: {self.email}, Image: {self.image}>'
     
@@ -29,6 +52,41 @@ class Activity(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     comments = db.relationship('Comment', backref='activity', lazy=True)
+    
+    serialize_rules = ('-comments.activity',)
+
+    @validates('name')
+    def validate_name(self, name):
+        if type(name) != str:
+            raise TypeError("Name must be a string")
+        elif len(name) < 3:
+            raise ValueError("Name must be at least 3 characters long")
+        else:
+            return name
+    
+    @validates('description')
+    def validate_description(self, description):
+        if type(description) != str:
+            raise TypeError("Description must be a string")
+        elif len(description) < 10:
+            raise ValueError("Description must be at least 10 characters long")
+        else:
+            return description
+        
+    @validates('datetime')
+    def validate_datetime(self, datetime):
+        if type(datetime) != datetime.datetime:
+            raise TypeError("Datetime must be a datetime object")
+        else:
+            return datetime
+
+    @validates('photos')
+    def validate_photos(self, photos):
+        if type(photos) != str:
+            raise TypeError("Photos must be a string")
+        else:
+            return photos
+
     def __repr__(self):
         return f'<Activity {self.name}, Description: {self.description}, Datetime: {self.datetime}, Photos: {self.photos}>'
 
@@ -41,6 +99,24 @@ class Comment(db.Model, SerializerMixin):
     activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
+    serialize_rules = ('-activity.comments', '-user.comments')
+    
+    @validates('content')
+    def validate_content(self, content):
+        if type(content) != str:
+            raise TypeError("Content must be a string")
+        elif len(content) < 10:
+            raise ValueError("Content must be at least 10 characters long")
+        else:
+            return content
+        
+    @validates('datetime')
+    def validate_datetime(self, datetime):
+        if type(datetime) != datetime.datetime:
+            raise TypeError("Datetime must be a datetime object")
+        else:
+            return datetime
+
     def __repr__(self):
         return f'<Comment {self.content}, Datetime: {self.datetime}, Activity: {self.activity_id}, User: {self.user_id}>'
     
