4eeefb1aec2dc6b7ea53ac1000475e0b3995742f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-18 15:54:04 -0600

    added /user-profile to routes.js, imported UserProfile into routes.js, added basic code to UserProfile.js, added Profile link to NavBar.js

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -7,6 +7,7 @@ function NavBar() {
         <nav>
             <NavLink to="/" className="nav-brand">Home</NavLink>
             <NavLink to="/activity-feed" className="nav-links">Activity Feed</NavLink>
+            <NavLink to="/user-profile" className="nav-links">Profile</NavLink>
         </nav>
     )
 }
