0cda0afa5c784673fba32fb8c9df4827bd4d7ecd
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-18 17:03:24 -0600

    Added login.css and signup.css and imported respectively, updated Login.js and SignUp.js to have more complex forms and classnames for styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,34 @@
+import "../../styling/login.css";
+
 function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
+  return (
+    <div className="login-container">
+      <div className="login-header">
+        <h1>Welcome Back</h1>
+        <p>Sign in to continue</p>
+      </div>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" />
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button className="login-button">Sign In</button>
+      </form>
+      <div className="login-footer">
+        <p>
+          Don't have an account? <a href="/signup">Sign up</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
