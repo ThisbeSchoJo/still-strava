b67576460462a5aa52d56f92d3df6814eb49b8c2
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-19 06:52:09 -0600

    Added simple form code to CommentForm.js

---
+++ client/src/components/comments/CommentForm.js
@@ -0,0 +1,18 @@
+
+
+function CommentForm() {
+    return (
+        <div>
+            <h1>Comment Form</h1>
+            <form>
+                <div>
+                    <label htmlFor="content">Content</label>
+                    <textarea id="content" name="content" />
+                </div>
+                <button type="submit">Create Comment</button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentForm;
