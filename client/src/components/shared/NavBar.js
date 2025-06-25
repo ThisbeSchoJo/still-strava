be89356109833856435f81696097d62f0ffe9c49
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-25 12:11:24 -0600

    debugged users/:id issue - needed to be made dynamic so active user id was retrieved

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -1,14 +1,21 @@
-import "../../styling/navbar.css";  
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 import { NavLink } from "react-router-dom";
-
+import "../../styling/navbar.css";  
 
 function NavBar() {
+    const { user } = useContext(UserContext);
+
     return (
         <nav>
             <NavLink to="/" className="nav-brand">Home</NavLink>
             <NavLink to="/activity-feed" className="nav-links">Activity Feed</NavLink>
-            <NavLink to="/users/:id" className="nav-links">Profile</NavLink>
-            <NavLink to="/login" className="nav-links">Login</NavLink>
+            {user && (
+                <NavLink to={`/users/${user.id}`} className="nav-links">Profile</NavLink>
+            )}
+            {!user && (
+                <NavLink to="/login" className="nav-links">Login</NavLink>
+            )}
         </nav>
     )
 }
