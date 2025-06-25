b7dc52f0d493006a41337feba9e6459f9fddcf0a
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-06-25 12:18:31 -0600

    added code to Logout

--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -1,4 +1,17 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+
 function Logout() {
+    const { setUser } = useContext(UserContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setUser(null);                          // Clear user from context
+        localStorage.removeItem("token");       // Remove token
+        navigate("/login");                     // Redirect to login
+    }, [setUser, navigate]);
+    
     return <p>Logging out...</p>; // Optional: replace with loading spinner
 }
 
