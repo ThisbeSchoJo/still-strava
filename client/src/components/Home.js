ec7e888edc0802b0fffd5fa5a50f77790b8f6a80
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-07-04 00:20:18 -0600

    AddedExplore Activities button to homepage and added styling to homepage in home.css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,16 @@
-function Home(){
-    return (
-        <div className="homepage">
-            <h2> Welcome to Still Strava</h2>
-            <p>
-                Go outside and log your time
-            </p>
+import "../styling/home.css";
+import { Link } from "react-router-dom";
 
-        </div>
-    )
+function Home() {
+  return (
+    <div className="homepage">
+      <h2>Welcome to Still Strava</h2>
+      <p>Go outside and log your time</p>
+      <Link to="/activity-feed" className="cta-button primary">
+        Explore Activities
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
