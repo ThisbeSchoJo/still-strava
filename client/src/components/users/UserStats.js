2bf4c21b2d7101d84dc44075dd8c646d8b42ac06
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-07-11 10:02:43 -0600

    Imported ChartJS components to UserStats.js and registered the ChartJS compoennts, added in some placeholder stats to test out the chart feature, also installed chart.js and react-chartjs-2 in client

--- client/src/components/users/UserStats.js
+++ client/src/components/users/UserStats.js
@@ -1,9 +1,45 @@
+// Import the ChartJS components
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Import the Bar component from react-chartjs-2
+import { Bar } from "react-chartjs-2";
 import "../../styling/userstats.css";
 
+// Register the ChartJS components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 function UserStats() {
-    return (
-        <h1>User Stats Chart/Graph will go here</h1>
-    )
+  const data = {
+    labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+    datasets: [
+      {
+        label: "Activities",
+        data: [3, 5, 2, 7],
+        backgroundColor: "#fc4c02", // Strava orange
+      },
+    ],
+  };
+  return (
+    <div className="user-stats-container">
+      <h1>User Stats Chart/Graph will go here</h1>
+      <Bar data={data} />
+    </div>
+  );
 }
 
 export default UserStats;
