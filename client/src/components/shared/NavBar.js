a7dbcb723f7540a5a8d47a6ad2a4b0022bb87208
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-07-14 17:33:45 -0600

    Added missing ARIA labels and roles to NavBar.js

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -1,26 +1,52 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import { NavLink } from "react-router-dom";
-import "../../styling/navbar.css";  
+import "../../styling/navbar.css";
 
 function NavBar() {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    return (
-        <nav>
-            <NavLink to="/" className="nav-brand">Home</NavLink>
-            <NavLink to="/activity-feed" className="nav-links">Activity Feed</NavLink>
-            {user && (
-                <>
-                    <NavLink to={`/users/${user.id}`} className="nav-links">Profile</NavLink>
-                    <NavLink to="/logout" className="nav-links">Logout</NavLink>
-                </>
-            )}
-            {!user && (
-                <NavLink to="/login" className="nav-links">Login</NavLink>
-            )}
-        </nav>
-    )
+  return (
+    <nav role="navigation" aria-label="Main navigation">
+      <NavLink to="/" className="nav-brand" aria-label="Go to home page">
+        Home
+      </NavLink>
+      <NavLink
+        to="/activity-feed"
+        className="nav-links"
+        aria-label="View activity feed"
+      >
+        Activity Feed
+      </NavLink>
+      {user && (
+        <>
+          <NavLink
+            to={`/users/${user.id}`}
+            className="nav-links"
+            aria-label="View your profile"
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to="/logout"
+            className="nav-links"
+            aria-label="Sign out of your account"
+          >
+            Logout
+          </NavLink>
+        </>
+      )}
+      {!user && (
+        <NavLink
+          to="/login"
+          className="nav-links"
+          aria-label="Sign in to your account"
+        >
+          Login
+        </NavLink>
+      )}
+    </nav>
+  );
 }
 
 export default NavBar;
