50487c086d4d2c0802251ee3b8db21b856b6daa9
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-07-22 13:12:36 -0700

    Added in state for followers and useEffect to fetch the followers to FollowersList.js

--- client/src/components/users/FollowersList.js
+++ client/src/components/users/FollowersList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getApiUrl } from "../../utils/api";
 
 function FollowersList({ userId, onClose }) {
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    fetch(getApiUrl(`/users/${userId}/followers`))
+      .then((res) => res.json())
+      .then((data) => {
+        setFollowers(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching followers:", error);
+      });
+  }, [userId]);
+
   return (
     <div>
       <h2>Followers</h2>
       <button onClick={onClose}>Close</button>
       <p>Followers list will go here</p>
+      <p>Number of followers: {followers.length}</p>
     </div>
   );
 }
